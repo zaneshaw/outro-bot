fb671e81eb8c59baa07192ee9e4f45e0cdbd2b85
Zane Shaw <admin@triangle100.com>
2022-09-21 20:50:40 +1000

    Add slash command

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 require("dotenv").config();
+require("./commandDeployer"); // Deploy commands
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent
     ]
 });
-const commands = [];
+client.commands = new Collection();
 
 client.once("ready", () => {
     console.log(`Bot ready (${client.user.tag})`);
 
     const commandsPath = path.join(__dirname, "commands");
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
-        commands.push(command);
+        client.commands.set(command.data.name, command);
     }
 })
 
 client.on("interactionCreate", async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    const command = commands.find((obj) => {
-        return obj.data.name === interaction.commandName;
-    });
+    const command = client.commands.get(interaction.commandName);
 
     if (!command) return;
 
