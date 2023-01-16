9bbc54062374c5b6a4afdb2304c6fb1258ff710e
Zane Shaw <admin@triangle100.com>
2023-01-16 12:27:40 +1100

    Fix merge errors

--- src/utils/log.js
+++ src/utils/log.js
@@ -2,13 +2,7 @@ const fs = require("node:fs");
 const stringify = require("json-stringify-safe");
 
 module.exports = {
-<<<<<<< HEAD
-	log(message, options = { time: true, serialise: true }) {
-		const { time, serialise } = options;
-=======
-	log(message, options = { time: true, track: true }) {
-		const { time, track: serialise } = options;
->>>>>>> 82b4365 (Fix circular object logging)
+	log(message, { log = true, time = true, serialise = true, subDir = "" } = {}) {
 		const date = new Date();
 		const splitDate = date.toLocaleString().split(", ");
 
@@ -20,14 +14,16 @@ module.exports = {
 
 		if (serialise) {
 			const name = splitDate[0].replace(/\//g, "-");
-			const dir = "logs/";
+			const dir = subDir == "" ? "logs/" : `logs/${subDir}/`;
+			const strMessage =
+				typeof message == "string" ? message : stringify(message);
 
 			if (!fs.existsSync(dir)) {
 				fs.mkdirSync(dir);
 			}
 			fs.appendFile(
 				`${dir}${name}.txt`,
-				`${timeStr} ${stringify(message)}\n`,
+				`${timeStr} ${strMessage}\n`,
 				(err) => {
 					if (err) throw err;
 
@@ -36,6 +32,6 @@ module.exports = {
 			);
 		}
 
-		console.debug(timeStr, message);
+		if (log) console.debug(timeStr, message);
 	},
 };
